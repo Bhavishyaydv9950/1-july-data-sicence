8faa01961d98bc6d263e9825e24f50c40900f9ec
Bhavishyaydv9950 <bhavishyayadav7597@gmail.com>
2025-07-12 15:31:39 +0530

    commit

---
+++ python/basic.py
@@ -0,0 +1,116 @@
+# print("hello")
+# print(PRINT)
+# age =22
+# Age =77
+# print(Age)
+# print(age)
+# string
+name="ritik"
+# indexing
+# print (name[0])
+# print (name [2])
+#  print(name[len(name)-1])
+# slicing in python 
+# print (name[0:4])
+# upper_case=name.upper()
+# print(upper_case)
+# lower_case=name.lower()
+#print(lower_case)
+# last="yadav"
+# lower_last_name=last.lower()
+# print(lower_last_name)
+# upper_last_name=last.upper()
+# print(upper_last_name)
+# print(name.count("r"))
+# print(name.title())
+# print(name.capitalize())
+# print(name+last)
+# list
+# lst=[1,3,4,5,7,8,"rohit"]
+# print(lst)
+# print(lst[0])
+# print(lst[6])
+# adding element
+a=[8]
+# a.append(2)
+# print("append(2):",a)
+# a.insert(1,5) 
+# print("after insert(1,5):",a)
+
+# print(lst[0:5])
+# lst=[1,2,2,2,3,4,5,6,"rohit",9,7,8]
+# lst.pop()
+# print(lst)
+# lst.reverse()
+# print(lst)
+# copy_lst=lst.copy()
+# print(copy_lst)
+# lst.reverse()
+# lst.remove(3)
+# print(lst)
+# lst.sort()
+# print(lst.count(3))
+# print(lst)
+# tuple
+# tpl=(1,2,3,4,5,[1,2,3,4,5,],"ritik",2.3)
+# print("my first tuple:-",tpl) 
+# print("len of tuple:-",len(tpl))
+# print ("type of tuple :-",type(tpl))
+# print(tpl[0:3])
+# a=1,2,58,5,58,5,5,85,
+# print(a)
+# print(type(a))
+# print(len(a))
+# a,b,c=1,2,3
+# print(a)a
+# print(b)
+# print(c)
+# tpl=(1,2,3,4,5,60)
+# print("max of tuple",max(tpl))
+# dictionary
+# my_dict={
+    "name":"rohit",
+    "class":"2nd year",
+    "roll number":"213br",
+    "address":"jaipur",
+}
+# print("my first dict:-",my_dict)
+# print("len of dict:-",len(my_dict))
+# print("type of dict:-",type(my_dict))
+# print(my_dict['name'])
+# print(my_dict['class'])
+# my_dict["address"]
+# task
+# lst=[1,3,4,5,[9,8,7,6],10,24]
+# print(lst[4][2])
+# list[2]=33
+# print(list)
+#get function
+# print(my_dict.keys())
+# print(my_dict.values())
+# print(my_dict.item())
+
+# copy_dict=my_dict.copy()
+# print(copy_dict)
+# my_dict.clear()
+# print(my_dict)
+# a=input("enter a number")
+# print(a)
+# type casting
+# a=int(input("enter a number"))
+# b=int(input("enter second number"))
+# print(a*b)
+
+
+# set
+my_set={1,2,3,4,5,6,7,"jai"}
+# print ("this my first set:-",my_set)
+# print("len of set:-",len(my_set))
+# print("type of set:-",(my_set))
+# lst=list(my_lst)
+# lst.append(1000)
+# print(set)(lst)
+
+
+
+  
